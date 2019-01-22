68bb03d95b03d3254982628ba1ab76275d516453
tut183328 <s183328@edu.tut.ac.jp>
2019-01-22 17:53:11 +0900

    not perfect subbyte

--- s4/B183328/InformationEstimator.java
+++ s4/B183328/InformationEstimator.java
@@ -1,4 +1,4 @@
-package s4.B183328; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID. 
+package s4.B183328; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID.
 import java.lang.*;
 import s4.specification.*;
 
@@ -11,8 +11,8 @@ public interface InformationEstimatorInterface{
 // It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
 // The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
 // Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity, 
-}                        
+// Otherwise, estimation of information quantity,
+}
 */
 
 public class InformationEstimator implements InformationEstimatorInterface{
@@ -22,22 +22,22 @@ public class InformationEstimator implements InformationEstimatorInterface{
     FrequencerInterface myFrequencer;  // Object for counting frequency
 
     byte [] subBytes(byte [] x, int start, int end) {
-	// corresponding to substring of String for  byte[] ,
-	// It is not implement in class library because internal structure of byte[] requires copy.
-	byte [] result = new byte[end - start];
-	for(int i = 0; i<end - start; i++) { result[i] = x[start + i]; };
-	return result;
+    	// corresponding to substring of String for  byte[] ,
+    	// It is not implement in class library because internal structure of byte[] requires copy.
+    	byte [] result = new byte[end - start];
+    	for(int i = 0; i<end - start; i++) { result[i] = x[start + i]; };
+    	return result;
     }
 
     // IQ: information quantity for a count,  -log2(count/sizeof(space))
     double iq(int freq) {
-	return  - Math.log10((double) freq / (double) mySpace.length)/ Math.log10((double) 2.0);
+	     return  - Math.log10((double) freq / (double) mySpace.length)/ Math.log10((double) 2.0);
     }
 
     public void setTarget(byte [] target) { myTarget = target;}
-    public void setSpace(byte []space) { 
-	myFrequencer = new Frequencer();
-	mySpace = space; myFrequencer.setSpace(space); 
+    public void setSpace(byte []space) {
+    	myFrequencer = new Frequencer();
+    	mySpace = space; myFrequencer.setSpace(space);
     }
 
     public double estimation(){
@@ -66,7 +66,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
 	    while(start<myTarget.length) {
 		// System.out.write(myTarget[end]);
 		end++;;
-		while(partition[end] == false) { 
+		while(partition[end] == false) {
 		    // System.out.write(myTarget[end]);
 		    end++;
 		}
@@ -102,8 +102,3 @@ public class InformationEstimator implements InformationEstimatorInterface{
 	System.out.println(">00 "+value);
     }
 }
-				  
-			       
-
-	
-    
